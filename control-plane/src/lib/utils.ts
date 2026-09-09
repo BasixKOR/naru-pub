becc4f64ea057354fe296ab8bba8f4af23292c13
Jihyeok Seo <ji@hyeok.org>
2026-09-09 09:41:53 +0900

    Make shared request import type-only

--- control-plane/src/lib/utils.ts
+++ control-plane/src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
